8cf01485af954fabd92b9dcd6ed6c7074d036e62
AntonBuus <antonbuushansen@gmail.com>
2025-05-09 16:29:22 +0200

    v3 connectionattempt 3

--- Assets/_Anton/Scripts/MissionTimeChecker.cs
+++ Assets/_Anton/Scripts/MissionTimeChecker.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class MissionTimeChecker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] AdjustablePrompts _adjustablePrompts; // Reference to the TTS system
+    [SerializeField] LeverTimeDial _leverTimeDial; // Reference to the lever time dial
+    public float _currentTime; // Current time in seconds
+    public float _middleTime = 180; // Time in seconds to warn the player
+    public float currentTimeFromSource; // This should be set from the external source
+    public string _winSceneName = "Win_Scene_v3" ; // Name of the win scene
+    public string _failSceneName = "Fail_Scene_v3"; // Name of the lose scene
+
     void Start()
     {
-        
+        if (_adjustablePrompts == null)
+        {
+            _adjustablePrompts = GameObject.Find("AdjustablePrompts").GetComponent<AdjustablePrompts>();
+        }
+        if (_leverTimeDial == null)
+        {
+            _leverTimeDial = GameObject.Find("Pull").GetComponent<LeverTimeDial>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // CheckMissionTime(); // Call the method to check the mission time
+    }
+
+    public void CheckMissionTime()
+    {
+        _currentTime = currentTimeFromSource; // Update _currentTime with the value from the external source
+        Debug.Log("called check missiontime"); // Log the current time for debugging
+        if (_currentTime <= 0)
+        {
+            _adjustablePrompts.SetTimeStatus(2); 
+            _adjustablePrompts.ObjectWasNotRetrieved(); 
+            _leverTimeDial.sceneToLoad = _failSceneName; // Set the scene to load to the lose scene
+        }
+        if (_currentTime <= _middleTime) // Check if the time is below x amount
+        {
+            _adjustablePrompts.SetTimeStatus(1); 
+            _leverTimeDial.sceneToLoad = _winSceneName; // Set the scene to load to the win scene
+        }
+        if (_currentTime > _middleTime) // Check if the time is above x amount
+        {
+            _adjustablePrompts.SetTimeStatus(0); 
+            _leverTimeDial.sceneToLoad = _winSceneName; // Set the scene to load to the win scene
+        }
     }
 }
