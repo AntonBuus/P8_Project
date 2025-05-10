528e23daccf3ec2d7a3377285b244315be62368f
AntonBuus <antonbuushansen@gmail.com>
2025-05-10 20:09:38 +0200

    Guard moves naturally now woop woop

--- Assets/_Anton/Scripts/AnimationRelated/DistractGuard.cs
+++ Assets/_Anton/Scripts/AnimationRelated/DistractGuard.cs
@@ -9,6 +9,7 @@ public class DistractGuard : MonoBehaviour
     public GameObject SceneObjectToEnable;
     
     public GameObject _distractableGuard; // Assign your stone prefab in the inspector
+    public TransformBetweenPoints _transformBetweenPoints;
     public Transform _newGuardPosition; // Assign your stone prefab in the inspector
 
     void Start()
@@ -20,7 +21,9 @@ public class DistractGuard : MonoBehaviour
         if (other.CompareTag("Stone"))
         {
             // Assuming you have a method to play the audio
-            audioSource.PlayOneShot(StoneHitSound);
+            // audioSource.PlayOneShot(StoneHitSound);
+            
+            RegularInvoke(); //Assign in the inspector what sounds to play
             // Destroy the stone after playing the sound
             Destroy(other.gameObject);
             // Enable the scene object 
@@ -28,9 +31,20 @@ public class DistractGuard : MonoBehaviour
             {
                 SceneObjectToEnable.SetActive(true);
             }
-            _distractableGuard.transform.position = new Vector3(_newGuardPosition.position.x, 
-            _distractableGuard.transform.position.y,_newGuardPosition.position.z); // Set the new position for the guard
+
+            _transformBetweenPoints.StartMoving(); // Start the movement of the guard
+
+            // _distractableGuard.transform.position = new Vector3(_newGuardPosition.position.x, 
+            // _distractableGuard.transform.position.y,_newGuardPosition.position.z); // Set the new position for the guard
         }
     }
 
+    public UnityEngine.Events.UnityEvent onInvoke;
+        private void RegularInvoke()
+    {
+        if (onInvoke != null)
+        {
+            onInvoke.Invoke();
+        }
+    }
 }
